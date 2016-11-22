d50d207c2a009d34ce08f81e9fd3ca4354e29f06
Michael Brown <michaelfordbrown@hotmail.com>
2016-11-22 16:50:47 +0000

    Add project files.

---
+++ FirstASPNETWebApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstASPNETWebApp.Startup))]
+namespace FirstASPNETWebApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
