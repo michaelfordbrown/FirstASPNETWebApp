2db6e8a0ea95ae2aecce048fd264a76bddd0a22a
Michael Brown <michaelfordbrown@hotmail.com>
2016-11-23 16:23:21 +0000

    Non working draft causing page load problems.

---
+++ FirstASPNETWebApp/ASPControls.aspx
@@ -0,0 +1,90 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ASPControls.aspx.cs" Inherits="FirstASPNETWebApp.WebForm1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div id="ACContent">
+        <h2>ASP Control Training Page</h2>
+        <div class="textBox">
+            <h3>ASP:Text Box</h3>
+            <h4>Tell us a little bit about yourself</h4>
+            <label>Name</label>
+            <asp:TextBox ID="txtName" runat="server" />
+            <asp:RequiredFieldValidator runat="server" ID="rfvName" ControlToValidate="txtName" ErrorMessage="Required." Display="Dynamic" />
+            <br />
+            <label>Email</label>
+            <asp:TextBox ID="txtEmail" runat="server" />
+            <asp:RegularExpressionValidator runat="server" ID="revEmail" ControlToValidate="txtEmail" ErrorMessage="Valid email address is required" ValidationExpression="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$" Display="Dynamic" />
+            <asp:RequiredFieldValidator runat="server" ID="rfvEmail" ControlToValidate="txtEmail" ErrorMessage="Required." Display="Dynamic" />
+            <br />
+        </div>
+        <div class="dropDownList">
+            <h3>ASP:Drop Down List</h3>
+            <label>Your Favorite Colour:</label>
+            <asp:DropDownList ID="ddlColour" runat="server">
+                <asp:ListItem Text="Please Choose A Colour" Value="" />
+                <asp:ListItem Text="Red" Value="Red" />
+                <asp:ListItem Text="Orange" Value="Orange" />
+                <asp:ListItem Text="Yellow" Value="Yellow" />
+                <asp:ListItem Text="Green" Value="Green" />
+                <asp:ListItem Text="Blue" Value="Blue" />
+                <asp:ListItem Text="Indigo" Value="Indigo" />
+                <asp:ListItem Text="Violet" Value="Violet" />
+            </asp:DropDownList>
+            <asp:RequiredFieldValidator runat="server" ID="rfvColour" ControlToValidate="ddlColour" ErrorMessage="Colour Required." Display="Dynamic" />
+            <br />
+        </div>
+        <div class="button">
+            <h3>ASP:Button</h3>
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit Info" OnClick="btnSubmit_Click" />
+            <br />
+        </div>
+        <div class="literal">
+            <h3>ASP:Literal</h3>
+            <asp:Literal ID="ltMessage1" runat="server" />
+            <br />
+        </div>
+        <div class="xMLFile">
+            <h3>ASP: XML File</h3>
+            <asp:DropDownList ID="ddxColours" runat="server">
+            </asp:DropDownList>
+            <br />
+            <h4>Colour Choosen From XML List: </h4>
+            <asp:Button ID="btnXMLColour" runat="server" Text="Submit Colour" OnClick="btnSubmit_Colour" />
+            <br />
+            <asp:Literal ID="liMessageXML1" runat="server" />
+            <br />
+        </div>
+        <div class="sqlDataSource">
+            <h3>ASP SQL Data Source</h3>
+            <p>Setup connection to the Azure Colours database.</p>
+            <asp:SqlDataSource ID="SqlDataSourceAzure" runat="server"
+                ConnectionString="<%$ ConnectionStrings:AzureColoursConnectionString %>"
+                SelectCommand="SELECT * FROM Colours"></asp:SqlDataSource>
+            <br />
+        </div>
+        <div class="gridView">
+            <h3>ASP:Grid View (Using the Azure Database)</h3>
+            <asp:GridView ID="GridView1" runat="server" DataSourceID="SqlDataSourceAzure">
+            </asp:GridView>
+            <br />
+            <asp:ListBox ID="lbColour" runat="server"></asp:ListBox>
+            <br />
+            <asp:Button ID="SelectColourButton" runat="server" OnClick="SelectColourButton_Click" Text="Display and Sort Colours" />
+            <br />
+            <h4>Selected Colour From Grid View: </h4>
+            <asp:Literal ID="ltMessageGV1" runat="server" />
+            <br />
+          </div>
+    </div>
+
+    <script type="text/javascript">
+        function changeColour() {
+            var div = document.getElementById("ACContent");
+            div.textContent = "Change Colour Called! Changing to: ";
+            var text = div.textContent;
+
+            ACContent.style.color = "red";
+        }
+    </script>
+</asp:Content>
+
---
+++ FirstASPNETWebApp/ASPControls.aspx.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace FirstASPNETWebApp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DataSet DSColours = new DataSet();
+                DSColours.ReadXml(Server.MapPath("App_Data\\Colours.xml"));
+
+                ddxColours.DataTextField = "ColourName";
+                ddxColours.DataValueField = "ColourId";
+                ddxColours.DataSource = DSColours;
+                ddxColours.DataBind();
+
+                ListItem liSelectColour = new ListItem("Select Colour", "-1");
+                ddxColours.Items.Insert(0, liSelectColour);
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string message = string.Format("<span style=color:{0};>You said your name is {1}, your email address is {2} and your favourite colour is {3}.</span>",
+               ddlColour.Text, txtName.Text, txtEmail.Text, ddlColour.SelectedValue);
+            ltMessage1.Text = message;
+        }
+        protected void btnSubmit_Colour(object sender, EventArgs e)
+        {
+            string message = string.Format("<span style=color:{0};>Colour Choosen is {0}.</span>",
+               ddxColours.SelectedItem.ToString());
+            liMessageXML1.Text = message;
+        }
+
+        protected void SelectColourButton_Click(object sender, EventArgs e)
+        {
+            SqlConnection colourConnection = new SqlConnection("Server=tcp:geminiserver.database.windows.net,1433;Initial Catalog=mfbColours;Persist Security Info=False;User ID=mike;Password=Metroe113llk;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            SqlCommand colourCommand = new SqlCommand("SELECT ColourName FROM Colours ORDER BY ColourName", colourConnection);
+
+            try
+            {
+                // if (SelectColourButton.Text.CompareTo("Display and Sort Colours") == 0)
+                {
+                    colourConnection.Open();
+                    SqlDataReader reader = colourCommand.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        lbColour.Items.Add(reader["ColourName"].ToString());
+                        //Response.Write("Azure Colour = "+ reader["ColourName"].ToString() +"<br />");
+                    }
+                    reader.Close();
+
+                    // SelectColourButton.Text = "Choose Colour";
+                }
+
+                string message = "Colour Choosen: ";
+                if (lbColour.SelectedItem != null)
+                {
+                    foreach (ListItem li in lbColour.Items)
+                    {
+                        if (li.Selected)
+                        {
+                            int intCR = li.Text.CompareTo("White ");
+
+                            if (intCR == 1)
+                            {
+                                message += string.Format("<span style=color:Black;> White</span><br/>");
+                            }
+                            else
+                            {
+                                message += string.Format("<span style=color:{0};> {0}</span><br/>", li.Text);
+                            }
+
+                        }
+                    }
+                    ltMessageGV1.Text = message;
+                }
+            }
+            catch (SqlException ex)
+            {
+                Response.Write("Select Colour Exception: " + ex.ToString());
+                throw;
+            }
+            finally
+            {
+                colourConnection.Close();
+            }
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /*  protected void LinkButton_Click(object sender, EventArgs e)
+          {
+              string message = string.Format("Control Items Font Colour Now:{0}",
+                  ddlColour.SelectedValue);
+              fcMessage.Text = message;
+
+              txtName.ForeColor = System.Drawing.ColorTranslator.FromHtml(ddlColour.Text);
+              txtEmail.ForeColor = System.Drawing.ColorTranslator.FromHtml(ddlColour.Text);
+              ddlColour.ForeColor = System.Drawing.ColorTranslator.FromHtml(ddlColour.Text);
+
+          } 
+
+          protected void ibSayHelloMax(object sender, EventArgs e)
+          {
+              string message = string.Format("<span style=color:{0};>Hello {1} my name is MAX and my favourite colour is {0}.</span>", "blue",txtName.Text);
+              ibMessage.Text = message;
+          } 
+          */
+
+          protected void btnSelectColours_Click(object sender, EventArgs e)
+          {
+              string message = "Colour Choosen:<br/>";
+              if (ddxColours.SelectedItem != null)
+              {
+                  foreach (ListItem li in ddxColours.Items)
+                  {
+                      if (li.Selected)
+                      {
+                          message += string.Format("<span style=color:{0};> {0}</span><br/>", li.Text);
+                      }
+                  }
+                liMessageXML1.Text = message;
+              }
+          }
+
+        /*
+          protected void btnGetData_Click(object sender, EventArgs e)
+          {
+
+          } */
+    }
+}
